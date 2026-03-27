ad4fe5ada3cec474ed57c725809287e4f50e1432
khasanov00 <xasanovnurilloh07@gmail.com>
2026-03-27 22:11:20 +0500

    Add Javascript and 0 Button
    I forgot 0 button last time and I added almost ready javascript(chain operating have some bugs still)

---
+++ script.js
@@ -0,0 +1,64 @@
+// 1. SELECT elements
+const buttons = document.querySelectorAll(".buttons");
+const display = document.querySelector("#display");
+const functionButtons=document.querySelectorAll(".functionButtons");
+const equalButton=document.querySelector("#equal");
+const numberButtons=document.querySelectorAll(".numberButtons");
+
+// 2. CREATE variables
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+// 3. CREATE functions
+function operate(num1, op, num2) {
+    num1=Number(num1);
+    num2=Number(num2);
+    if (op==="/" && num2===0) return "error";
+    if (op === "+") return num1 + num2;
+    if (op === "-") return num1 - num2;
+    if (op === "*") return num1 * num2;
+    if (op === "/") return num1 / num2;
+    
+}
+
+// 4. ADD event listeners
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        display.value+=button.textContent;
+    });
+});
+
+// 5. UPDATE display
+const clear=document.querySelector("#clear");
+
+clear.addEventListener("click", ()=>{
+    display.value="";
+});
+
+// 6. OPERATE
+
+functionButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (firstNumber !== "" && display.value !== "") {
+            secondNumber = display.value;
+            let result = operate(firstNumber, operator, secondNumber);
+            display.value = result;
+            firstNumber = result;
+        } else {
+            firstNumber = display.value;
+        }
+
+        operator = button.textContent;
+        display.value = "";
+    });
+});
+equalButton.addEventListener("click",()=>{
+    secondNumber=display.value;
+   console.log("first:", firstNumber);
+    console.log("operator:", operator);
+    console.log("second:", secondNumber);
+
+    let result = operate(firstNumber, operator, secondNumber);
+    display.value = result;
+});
